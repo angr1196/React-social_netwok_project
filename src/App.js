41f02bed70be6c089b77c1f98b4312d378e5eaf8
Oleksiy Gron <gronaleksey@gmail.com>
2023-02-09 01:36:47 +0200

    new updates for profileContiner

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ const App = () => {
       <Navbar />
       <div className="content-wrapper" >
         <Routes>
-          <Route path='/profile*' element={<ProfileContainer/>} />
+          <Route path='/profile/:userId' element={<ProfileContainer />} />
+          <Route path='/profile/' element={<ProfileContainer />} />
           <Route path="/dialogs/*" element={<DialogsContainer />} />
           <Route path="/users" element={<UsersContainer />} />
           <Route path="/news" element={<News />} />
